30e6c12309dfad884d3ca651a34bd04398e207be
mehddii <mehdi7bs@gmail.com>
2026-03-15 13:10:59 +0000

    test: Extend test to cover a bigger set of tokens

--- tokenizer/tokenizer_test.go
+++ tokenizer/tokenizer_test.go
@@ -5,23 +5,81 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := "=+-*/({});"
+	input := `
+	def a = 4;
+	def f = func(x) {
+		return 2 * x;
+	};
+
+	f(a);
+
+	def add = func(x, y) {
+		return x + y;
+	};
+
+	add(3, 5);
+	`
 	tokenizer := NewTokenizer(input)
 
 	tests := []struct {
 		Type    TokenType
 		Literal string
 	}{
+		{DEF, "def"},
+		{IDENT, "a"},
 		{ASSIGN, "="},
-		{PLUS, "+"},
-		{MINUS, "-"},
-		{ASTERISK, "*"},
-		{SLASH, "/"},
+		{INTEGER, "4"},
+		{SEMICOLON, ";"},
+
+		{DEF, "def"},
+		{IDENT, "f"},
+		{ASSIGN, "="},
+		{FUNC, "func"},
 		{LPAREN, "("},
+		{IDENT, "x"},
+		{RPAREN, ")"},
 		{LBRACE, "{"},
+		{RETURN, "return"},
+		{INTEGER, "2"},
+		{ASTERISK, "*"},
+		{IDENT, "x"},
+		{SEMICOLON, ";"},
 		{RBRACE, "}"},
+		{SEMICOLON, ";"},
+
+		{IDENT, "f"},
+		{LPAREN, "("},
+		{IDENT, "a"},
 		{RPAREN, ")"},
 		{SEMICOLON, ";"},
+
+		{DEF, "def"},
+		{IDENT, "add"},
+		{ASSIGN, "="},
+		{FUNC, "func"},
+		{LPAREN, "("},
+		{IDENT, "x"},
+		{COMMA, ","},
+		{IDENT, "y"},
+		{RPAREN, ")"},
+		{LBRACE, "{"},
+		{RETURN, "return"},
+		{IDENT, "x"},
+		{PLUS, "+"},
+		{IDENT, "y"},
+		{SEMICOLON, ";"},
+		{RBRACE, "}"},
+		{SEMICOLON, ";"},
+
+		{IDENT, "add"},
+		{LPAREN, "("},
+		{INTEGER, "3"},
+		{COMMA, ","},
+		{INTEGER, "5"},
+		{RPAREN, ")"},
+		{SEMICOLON, ";"},
+
+		{EOF, ""},
 	}
 
 	for _, test := range tests {
@@ -32,7 +90,7 @@ func TestNextToken(t *testing.T) {
 		}
 
 		if tok.Literal != test.Literal {
-			t.Fatalf("expected %v; got %v", test.Literal, tok.Literal)
+			t.Fatalf("expected %q; got %q", test.Literal, tok.Literal)
 		}
 	}
 }
